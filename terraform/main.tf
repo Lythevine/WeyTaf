fe509e06048f696eaa9ccabbdf3090f171f5c94b
LythevineDomche <lythevinedomche@gmail.com>
2023-07-11 18:49:56 +0100

    second

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,29 @@ resource "kubernetes_pod_v1" "my-test" {
     }
   }
 }
+#second pod
+resource "kubernetes_pod_v1" "my-test2" {
+  metadata {
+    name = "mysecond-pod"
+    namespace = "rashid"
+  }
+  spec {
+    container {
+      name = "marwalythe2"
+      image = "lythevine/mymarwaney:my_marwaney"
+    }
+  }
+}
+#third pod
+resource "kubernetes_pod_v1" "my-test3" {
+  metadata {
+    name = "mythird-pod"
+    namespace = "rashid"
+  }
+  spec {
+    container {
+      name = "marwalythe3"
+      image = "lythevine/mymarwaney:my_marwaney"
+    }
+  }
+}
